95c7d47fdc3f8ef79c867a18d47875c3d22fac16
Jon Ruskin <jonabc@github.com>
2021-04-20 17:09:24 -0700

    try to install gem, then try to install exe

--- tests/setup-licensed.test.js
+++ tests/setup-licensed.test.js
@@ -14,7 +14,8 @@ describe('setup-licensed', () => {
     sinon.stub(core, 'addPath');
     sinon.stub(core, 'info');
 
-    sinon.stub(installers, 'exe').resolves();
+    sinon.stub(installers, 'gem').resolves(version);
+    sinon.stub(installers, 'exe').resolves(version);
 
     process.env = {
       ...process.env,
@@ -43,19 +44,54 @@ describe('setup-licensed', () => {
     expect(core.setFailed.getCall(0).args).toEqual(['abc is not a valid semantic version input']);
   });
 
-  it('installs licensed as a standalone executable', async () => {
+  it('installs licensed from a gem', async () => {
     await run();
     expect(core.setFailed.callCount).toEqual(0);
+
+    expect(core.info.callCount).toEqual(2);
+    expect(core.info.getCall(0).args).toEqual([`attempting to install licensed gem matching "${version}"`]);
+    expect(core.info.getCall(1).args).toEqual([`licensed (${version}) gem installed`]);
+
+    expect(installers.gem.callCount).toEqual(1);
+    expect(installers.gem.getCall(0).args).toEqual([version]);
+
+    expect(installers.exe.callCount).toEqual(0);
+  });
+
+  it('installs licensed as a standalone executable if gem install failed', async () => {
+    installers.gem.resolves(null);
+
+    await run();
+    expect(core.setFailed.callCount).toEqual(0);
+
+    expect(core.info.callCount).toEqual(3);
+    expect(core.info.getCall(0).args).toEqual([`attempting to install licensed gem matching "${version}"`]);
+    expect(core.info.getCall(1).args).toEqual([`attempting to install licensed executable matching "${version}"`]);
+    expect(core.info.getCall(2).args).toEqual([`licensed (${version}) executable installed`]);
+
+    expect(installers.gem.callCount).toEqual(1);
 
     expect(installers.exe.callCount).toEqual(1);
     expect(installers.exe.getCall(0).args).toEqual([version]);
   });
 
   it('sets a failure when installation raises an error', async () => {
-    installers.exe.rejects(new Error('test failure'));
+    installers.gem.rejects(new Error('test failure'));
 
     await run();
     expect(core.setFailed.callCount).toEqual(1);
     expect(core.setFailed.getCall(0).args).toEqual(['test failure']);
+  });
+
+  it('sets a failure when installation fails', async () => {
+    installers.gem.resolves(null);
+    installers.exe.resolves(null);
+
+    await run();
+    expect(core.setFailed.callCount).toEqual(1);
+    expect(core.setFailed.getCall(0).args).toEqual([`unable to install licensed matching "${version}"`]);
+    expect(core.info.callCount).toEqual(2);
+    expect(core.info.getCall(0).args).toEqual([`attempting to install licensed gem matching "${version}"`]);
+    expect(core.info.getCall(1).args).toEqual([`attempting to install licensed executable matching "${version}"`]);
   });
 });
